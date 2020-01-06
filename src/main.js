ba2229384a84a8fb29c94d3516db768316f134d7
leegeunhyeok <dev.ghlee@gmail.com>
2020-01-06 18:36:11 +0900

    UPDATE: Add IndexedDB Manger to Vue.prototype

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import axios from 'axios'
+import dataStore from './data-store'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
+Vue.prototype.$db = dataStore()
 
 new Vue({
   render: h => h(App)
