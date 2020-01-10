c6316fead4e300e6adc9545b9f3736d15fa085d7
leegeunhyeok <dev.ghlee@gmail.com>
2020-01-10 13:23:04 +0900

    UPDATE: Add blog icon

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import {
+  faEnvelope,
+  faBlog
+} from '@fortawesome/free-solid-svg-icons'
 import {
   faGithub,
   faLinkedin,
@@ -11,6 +14,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add([
   faEnvelope,
+  faBlog,
   faGithub,
   faLinkedin,
   faInstagram
